592499d3b58c11db7a5492643df2b88a59d19e92
swarnimbungrai <raiswornimbung@gmail.com>
2023-08-03 14:17:54 +0545

    installed Multer, middleware which handles file uploads from html form

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const {sequelize, blogTB} = require ('./model')
 const bcrypt = require("bcrypt");
 const { registerBlog, loginBlog, forgotPassword, otpConfirm, blogAdd } = require('./controller/authController');
 
-
+//multer, package junle image/file upload grnu dinxa
+const { multer, storage } = require("././service/multerConfig");
+const upload = multer({ storage: storage });
 
 
 //shows data in terminal which were filled in form
@@ -53,7 +55,8 @@ app.post('/confirm',otpConfirm)
 app.get('/blog',(req,res) => {
    res.render('blog.ejs')
 })
-app.post('/blog',blogAdd)
+                 //middleware of multer
+app.post('/blog', upload.single('image'), blogAdd)
 
 
 app.listen(4000,()=>{
